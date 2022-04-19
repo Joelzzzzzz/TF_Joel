9da965cd7a4b056b0c19d5d385b8bb268fb6db45
joel_ <joel_@LAPTOP-SAANVEMP>
2022-04-19 00:45:26 -0500

    commit_Joel

--- src/main/java/pe/edu/upc/daoimpls/PosteoImpl.java
+++ src/main/java/pe/edu/upc/daoimpls/PosteoImpl.java
@@ -1,5 +1,26 @@
 package pe.edu.upc.daoimpls;
 
-public class PosteoImpl {
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
 
+import pe.edu.upc.daointerfaces.IPosteoDao;
+import pe.edu.upc.entities.Posteo;
+
+public class PosteoImpl implements IPosteoDao {
+	@PersistenceContext(unitName = "TF_Joel")
+	private EntityManager em;
+	
+	@Transactional
+	@Override
+	public void insert(Posteo p) {
+
+		try {
+			em.persist(p);
+		} catch (Exception e) {
+			System.out.println("Error al insertar persona en DAO");
+
+		}
+
+	}
 }
