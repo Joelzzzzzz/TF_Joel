3333d25dd6d0d783dc6c2f9a316220ccc4745b60
joel_ <joel_@LAPTOP-SAANVEMP>
2022-04-19 00:57:58 -0500

    commit_Joel

--- src/main/java/pe/edu/upc/daoimpls/PosteoImpl.java
+++ src/main/java/pe/edu/upc/daoimpls/PosteoImpl.java
@@ -1,7 +1,11 @@
 package pe.edu.upc.daoimpls;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 import javax.transaction.Transactional;
 
 import pe.edu.upc.daointerfaces.IPosteoDao;
@@ -18,9 +22,33 @@ public class PosteoImpl implements IPosteoDao {
 		try {
 			em.persist(p);
 		} catch (Exception e) {
-			System.out.println("Error al insertar persona en DAO");
+			System.out.println("Error al insertar posteo en DAO");
 
 		}
 
+	}
+	
+	@SuppressWarnings("unchecked")
+	@Override
+	public List<Posteo> list(){
+		List<Posteo> listaPosteos=new ArrayList<Posteo>();
+		try {
+			Query jpql=em.createQuery("from Posteo p");
+			listaPosteos=(List<Posteo>) jpql.getResultList();
+		} catch (Exception e) {
+			System.out.println("Error al listar en el DAO");
+		}
+		return listaPosteos;
+	}
+	
+	@Transactional
+	@Override
+	public void delete(int id) {
+		try {
+			Posteo per=em.find(Posteo.class, id);
+			em.remove(per);
+		} catch (Exception e) {
+			System.out.println("Error al eliminar en el DAO");
+		}
 	}
 }
